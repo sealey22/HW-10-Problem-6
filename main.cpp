6964de6b810509bc034c6f375e1c386847d7e2c6
Hsealey <112498796+sealey22@users.noreply.github.com>
2022-11-02 00:39:32 +0000

    setup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
-}
+
+  struct car
+  {
+  int number;
+  int miles;
+  int gallons;
+ } ;
+
+
+  cout << " Enter car number: ";
+  cin >> car.number;
+
+  cout << " Enter miles driven: ";
+  cin >> car.miles;
+
+  cout << "Enter gallons: ";
+  cin >> car.gallons;
+
+  cout << " ------------------------------------------------------------";
+
+  cout << "\nCar number: " << " " << car.number << "\n" <<
+    "Gallons used: " << " " << car.miles << "\n" << 
+    "Miles driven: " << " " << car.gallons << endl;
+  
+  
+  
+  return 0;
+  
+  }
